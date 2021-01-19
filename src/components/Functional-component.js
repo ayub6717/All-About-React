c84c2935972583ffb639f99779f8b6d8ab003cd6
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-19 22:00:09 +0600

    Functional Component

--- src/components/Functional-component.js
+++ src/components/Functional-component.js
@@ -4,6 +4,10 @@ import React from 'react'
 //    return <h1>Hello Ayub</h1>
 // }
 
-const FunctionalComponent = () => <h1>Hello Ayub</h1>
+const FunctionalComponent = () => 
+<div>
+   <h1>Hello Ayub</h1>
+   <p>Hey I'm Ayub. I'am a professonal Front-End Developer</p>
+</div>
 
 export default FunctionalComponent
