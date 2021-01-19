e61b1079704ac7e0b2b5c8309d939774b3265b69
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-19 22:08:49 +0600

    Functional Component

--- src/components/Functional-component.js
+++ src/components/Functional-component.js
@@ -8,6 +8,18 @@ const FunctionalComponent = () =>
 <div>
    <h1>Hello Ayub</h1>
    <p>Hey I'm Ayub. I'am a professonal Front-End Developer</p>
+
+   <div className="row">
+      <div className="col-sm-3" style={{height:"300px"}}>
+         <h1>Hello</h1>
+      </div>      
+      <div className="col-sm-3">
+         <h1>Hi</h1>
+      </div>      
+      <div className="col-sm-3">
+         <h1>By</h1>
+      </div>
+   </div>
 </div>
 
 export default FunctionalComponent
